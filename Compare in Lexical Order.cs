f2ab92cde3af20bb14d29489455b423d3e81217b
Burak SALTALI <96692734+bsaltalii@users.noreply.github.com>
2023-01-24 15:27:01 +0300

    Add files via upload

---
+++ Compare in Lexical Order.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter the first string : ");
+string wordOne = Console.ReadLine();
+char[] arrayOne = wordOne.ToCharArray();
+
+Console.Write("Enter the first string : ");
+string wordTwo = Console.ReadLine();
+char[] arrayTwo = wordTwo.ToCharArray();
+
+for( int i = 0; i < wordOne.Length; i++)
+{
+    if (arrayOne[i]< arrayTwo[i])
+    {
+        Console.WriteLine("First string comes before second string");
+        break;
+    }
+    else if (arrayTwo[i] < arrayOne[i])
+    {
+        Console.WriteLine("Second string comes before first string");
+        break;
+    }
+
+}
---
+++ Longest Consecutive Equal Elements.cs
@@ -0,0 +1,48 @@
+﻿int lengthOfArray,counterOne,counterTwo,number;
+counterOne = 1;
+counterTwo = 1;
+number = 0;
+
+Console.Write("Enter the length of the array : ");
+lengthOfArray = int.Parse(Console.ReadLine());
+
+int[] array = new int[lengthOfArray];
+
+for(int i = 0; i<lengthOfArray; i++)
+{
+    Console.Write("Enter the integer : ");
+    array[i] = int.Parse(Console.ReadLine());
+
+}
+for(int j = 0; j < array.Length - 1; j++)
+{
+    if (array[j] == array[j + 1])
+    {
+        counterOne++;
+    }
+    else
+    {
+        counterOne = 1;
+    }
+    if (counterOne > counterTwo)
+    {
+        counterTwo = counterOne;
+        number = array[j];
+    }
+}
+
+Console.Write("Maximal sequence of consecutive equal elements { ");
+
+for(int i = 0; i < counterTwo; i++)
+{
+    Console.Write(" {0} ", number);
+    if (i == (counterTwo - 1))
+    {
+        continue;
+    }
+    else
+    {
+        Console.Write(",");
+    }
+}
+Console.Write("}");
